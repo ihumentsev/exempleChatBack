0a971948ada7772f4a572b86ac5f001447777141
ihumentsev <a.ihumentsev@gmail.com>
2022-12-27 21:49:53 +0100

    ..

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 // Создание сервера
 const server = require("http").createServer(app);
 // Берём API socket.io
